b4f26694525a645bd787603174d4cbe183b7a3eb
TombojaraElyse <tombojara.elyse@gmail.com>
2021-10-01 19:23:53 +0300

    Stopping at 4:26:43

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,8 +3,11 @@ import { useParams } from 'react-router-dom';
 // Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // Components
+import BreadCrumb from './BreadCrumb';
 import Grid from './Grid';
 import Spinner from './Spinner';
+import MovieInfo from './MovieInfo';
+import MovieInfoBar from './MovieInfoBar';
 // Hook
 import { useMovieFetch } from '../hooks/useMovieFetch';
 // Image
@@ -15,11 +18,18 @@ const Movie = () => {
 
   const { state: movie, loading, error } = useMovieFetch(movieId);
 
-  console.log(movie);
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong ...</div>;
 
   return (
     <>
-      <div>Movie</div>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
     </>
   );
 };

f8d8392f6fe9edfb7c9b35879eaaeeed80482b15
TombojaraElyse <71205123+TombojaraElyse@users.noreply.github.com>
2022-12-01 15:57:17 +0300

    update config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 
 const API_URL = 'https://api.themoviedb.org/3/';
 //const API_KEY = process.env.REACT_APP_API_KEY;
-const API_KEY = '06bdf6ff57ed573cb8deb0dc9056b680';
+const API_KEY = '';
 
 const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`;
 const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`;
